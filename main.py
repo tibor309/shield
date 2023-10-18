25b4dfe911eb6a83faa3c253372dac38d7d08760
Tibor Mercs <tibormercs8@gmail.com>
2023-10-18 20:28:30 +0200

    Added a todo list and a requirements file

---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO:
+# moderation commands (kick, ban, etc.)
+# timeout members
+# purge messages
+# give/revoke roles
+# delete server invites
+# lock/unlock channel
+# set nickname for a member
+# softban members
+# un/deafen members
+# mute members
+# member info
