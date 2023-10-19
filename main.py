5a0ffcdd0583d7204399d594381b950ae6c33879
Tibor Mercs <tibormercs8@gmail.com>
2023-10-19 13:30:26 +0200

    Created bot

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import discord
+from discord.ext import commands
+from config import bot_token
+
+intents = discord.Intents.default()
+bot = commands.Bot(intents=intents, help_command=None)
+
+
+@bot.event
+async def on_ready():
+    print(f"Successfully logged in as {bot.user}")
+
+
+# Make bot not respond to it's owm messages
+@bot.listen
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+bot.run(bot_token)
+
 # TODO:
 # moderation commands (kick, ban, etc.)
 # timeout members
