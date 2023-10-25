483add7cddf45299fad520f9b6c444581559bb73
Tibor Mercs <tibormercs8@gmail.com>
2023-10-25 15:00:36 +0200

    Added icons

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ load_dotenv()
 bot_token = os.getenv("TOKEN")
 bot_time = "%d/%b/%Y %H:%M:%S"
 bot_color = 0xa6e3a1
+
+# Icons
+member_icon = "https://i.imgur.com/DOP6Tei.png"
+gear_icon = "https://i.imgur.com/1DI2lNu.png"
