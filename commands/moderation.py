410ff9e148efcf1f8b062f45f24a6bbf17462e1f
Tibor Mercs <tibormercs8@gmail.com>
2023-10-26 08:57:32 +0200

    Added softban command

--- commands/moderation.py
+++ commands/moderation.py
@@ -62,5 +62,33 @@ class moderation(commands.Cog):
 
 
 
+    @discord.slash_command(name="softban", description="Softban a member (ban and unban to delete messages)", guild_only=True)
+    @discord.commands.default_permissions(ban_members=True)
+    @discord.option("member", discord.Member, description="Select a member", required=True)
+    @discord.option("reason", str, description="Add a reason (optional)", required=False)
+    async def softban(self, ctx: commands.Context, member: discord.Member, reason: str = "*No reason given*") -> None:
+        create_time = int(member.created_at.timestamp())
+        join_time = int(member.joined_at.timestamp())
+
+        if member == ctx.author:
+            return await ctx.respond(f"You can't softban yourself", ephemeral=True)
+        elif member == self.bot.user:
+            return await ctx.respond("Nope", ephemeral=True)
+
+        embed = discord.Embed(color=bot_color, title=f"{member.name} has been softbanned!", description=f"**Reason:**\n{reason}", timestamp=discord.utils.utcnow())
+        embed.set_author(name="Member softbanned", icon_url=member_icon)
+        embed.add_field(name="Account Created", value=f"<t:{create_time}:R>")
+        embed.add_field(name="Joined Server", value=f"<t:{join_time}:R>")
+        embed.set_thumbnail(url=member.avatar)
+
+        try:
+            await member.ban(reason=f"{reason} - softbanned by @{ctx.author.name}", delete_message_seconds=604800)
+            await member.unban(reason=f"{reason} - softbanned by @{ctx.author.name}")
+            await ctx.respond(embed=embed)
+        except:
+            await ctx.respond(f"Fauled to softban {member.mention}! Probably missing permissions, or you're trying to softban someone higher than me", ephemeral=True)
+
+
+
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(moderation(bot))
